d9e96211d42f52ec045b47bff9ecbd129b1ef551
melvyndev <154402936+melvyndev@users.noreply.github.com>
2024-08-24 10:57:40 +0400

    card product

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
+import { products } from "@/utils/product"; // Assuming products is an array of objects
 import Container from "./components/Container";
 import HomeBanner from "./components/nav/HomeBanner";
+import Card from "./components/Card";
+
+
 
 export default function Home() {
   return (
-  <div>
-  <Container>
-      <HomeBanner/>
-  </Container>
-
-  </div>
-  )
+    <div>
+      <Container>
+        <HomeBanner />
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {products.map((product: any) => {
+            return (
+           <Card key={product.id} product={product} />
+            );
+          })}
+        </div>
+      </Container>
+    </div>
+  );
 }
